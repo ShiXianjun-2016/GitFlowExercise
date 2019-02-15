ecfb1eb646363b4160c7dd436e9bdd8fe6c08138
ShiXianjun <1092207960@qq.com>
2019-02-15 10:55:23 +0800

    登录功能完成

---
+++ GitFlowExercise/GitFlowExercise/Codetree/Login/Controllers/UIControllerLogin.h
@@ -0,0 +1,17 @@
+//
+//  UIControllerLogin.h
+//  GitFlowExercise
+//
+//  Created by 石显军 on 2019/2/15.
+//  Copyright © 2019 石显军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIControllerLogin : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GitFlowExercise/GitFlowExercise/Codetree/Login/Controllers/UIControllerLogin.m
@@ -0,0 +1,33 @@
+//
+//  UIControllerLogin.m
+//  GitFlowExercise
+//
+//  Created by 石显军 on 2019/2/15.
+//  Copyright © 2019 石显军. All rights reserved.
+//
+
+#import "UIControllerLogin.h"
+
+@interface UIControllerLogin ()
+
+@end
+
+@implementation UIControllerLogin
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
